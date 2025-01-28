0cf2f8728a4d76e5fcd0ce3e397b2a26183c637c
Marvin De la cruz <marvindelacruz2004@gmail.com>
2025-01-28 15:52:37 -0600

    three

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
+
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;  
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'))
 
-// change my code 
+// change my code
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html');
+res.sendFile('index.html');
 
 })
 
+app.post('/saveMyName', (req,res)=>{
+console.log('did we hit the post endpoint?');
+
+console.log(req.body);
+
+// res.redirect('/ejs');
+
+res.render('words',
+{pageTitle: req.body.myName});
+
+// res.render('words',
+// {theData: req.body})
+})
+
+app.get('/saveMyNameGet', (req,res)=>{
+console.log('did we hit the get endpoint?');
+
+console.log(req.query);
+
+res.redirect('/ejs');
+
+})
 
 app.get('/ejs', function (req, res) {
-  res.render('words',
-    {pageTitle: 'my cool ejs page'}
-  );
+res.render('words',
+{pageTitle: 'my cool ejs page'}
+);
 })
 
-
 app.get('/nodemon', function (req, res) {
-  res.send('look ma, no kill node process then restart node then refresh browser...cool?');
+res.send('look ma, no kill node process then restart node then refresh browser...cool?');
 
 })
 
 //endpoint, middleware(s)
 app.get('/helloRender', function (req, res) {
-  res.send('Hello Express from Real World<br><a href="/">back to home</a>')
+res.send('Hello Express from Real World<br><a href="/">back to home</a>')
 })
 
-
-
-
 app.listen(
-  port, 
-  ()=> console.log(
-    `server is running on ... ${port}`
-    )
-  );
+port,
+()=> console.log(
+`server is running on ... ${port}`
+)
+);
